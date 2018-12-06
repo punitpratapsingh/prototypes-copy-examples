84146760d3e650c1bf8c4a6b02b0b2f59cd45b89
punit <punitpratapsingh30@gmail.com>
2018-12-06 19:18:34 +0530

    updated with generic

--- task1.js
+++ task1.js
@@ -1,12 +1,6 @@
-function vowels(str){
-    this.str=str;
-}
-vowels.prototype.countVowel= function(){
+String.prototype.countVowel= function(){
 
-    var m = this.str.match(/[aeiou]/gi);
-   m === null ? 0 : m.length;
-  console.log(m.length);
-   }
-
-var vowels1 = new vowels('hello');
-vowels1.countVowel();
+    var m = this.match(/[aeiou]/gi);
+  return m === null ? 0 : m.length;
+     }
+console.log("aeiou" .countVowel())
